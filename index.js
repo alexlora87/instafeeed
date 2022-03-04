2e97a6e051bd14b17c41de6aeed3e3b8d1a19259
Alejandro Lora <alexlora87@gmail.com>
2022-03-04 15:51:41 -0300

    iteration_8

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const articleRouter = require("./routers/article");
+const mongoExpressReq = require("mongo-express-req");
 
-const app = express();
-const port = process.env.PORT || 8080;
+// Connection data
+const url = "mongodb://127.0.0.1:27017/instafeed";
+const config = { useNewUrlParser: true };
+// Create a new MongoClient
+const client = new MongoClient(url, config);
 
-app.use(express.json());
-app.use(articleRouter);
+const startExpress = () => {
+  const app = express();
+  const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
-});
+  app.use(express.json());
+  app.use(mongoExpressReq(url, config));
+  app.use(articleRouter);
+  app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+  });
+};
+
+const startServer = async () => {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    startExpress();
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+};
+
+startServer();
--- routers/article.js
+++ routers/article.js
@@ -1,48 +1,48 @@
-const fs = require("fs");
 const express = require("express");
 const router = new express.Router();
 const { joiValidation } = require("../validation");
-const editJsonFile = require("edit-json-file");
 
-const dataBuffer = fs.readFileSync(`./db.json`);
-const dataBase = JSON.parse(dataBuffer);
-const articles = dataBase.db;
-
-const writeJSONFile = (name, data) => {
-  const jsonFile = editJsonFile(`${name}.json`);
-  jsonFile.append(name, data);
-  jsonFile.save();
-};
+const dbName = "instafeed";
+const articleCollection = (req) => req.db.db(dbName).collection("articles");
 
 router.post("/articles", async (req, res) => {
   const _article = req.body;
 
   try {
     joiValidation(_article);
-    articles.push(_article);
-    writeJSONFile("db", _article);
+    await articleCollection(req).insertOne(_article);
     res.status(201).send(_article);
   } catch (e) {
-    writeJSONFile("invalid", _article);
+    console.log(e);
     res.status(400).send(e);
   }
 });
 
-router.get("/articles", async (_, res) => {
-  if (articles.length > 0) {
-    res.send(articles);
-  } else {
-    res.status(404).send();
+router.get("/articles", async (req, res) => {
+  try {
+    var articles = await articleCollection(req).find().toArray();
+    if (articles.length > 0) {
+      res.send(articles);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    res.status(400).send();
   }
 });
 
 router.get("/articles/:id", async (req, res) => {
   const _id = req.params.id;
-  const article = articles.filter(({ id }) => id === _id);
-  if (article.length > 0) {
-    res.send(article);
-  } else {
-    res.status(404).send();
+
+  try {
+    var articles = await articleCollection(req).find({ id: _id }).toArray();
+    if (articles.length > 0) {
+      res.send(articles);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    res.status(400).send();
   }
 });
 
