8164aaac3c2fa7d82030a7a44356fd036d4777d1
Alejandro Lora <alexlora87@gmail.com>
2022-03-04 00:32:15 -0300

    iteration_7

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use(articleRouter);
 
 app.listen(port, () => {
-  console.log("Server is up on port " + port);
+  console.log(`Server is up on port ${port}`);
 });
--- routers/article.js
+++ routers/article.js
@@ -1,19 +1,49 @@
 const fs = require("fs");
 const express = require("express");
 const router = new express.Router();
+const { joiValidation } = require("../validation");
+const editJsonFile = require("edit-json-file");
 
 const dataBuffer = fs.readFileSync(`./db.json`);
 const dataBase = JSON.parse(dataBuffer);
 const articles = dataBase.db;
 
+const writeJSONFile = (name, data) => {
+  const jsonFile = editJsonFile(`${name}.json`);
+  jsonFile.append(name, data);
+  jsonFile.save();
+};
+
+router.post("/articles", async (req, res) => {
+  const _article = req.body;
+
+  try {
+    joiValidation(_article);
+    articles.push(_article);
+    writeJSONFile("db", _article);
+    res.status(201).send(_article);
+  } catch (e) {
+    writeJSONFile("invalid", _article);
+    res.status(400).send(e);
+  }
+});
+
 router.get("/articles", async (_, res) => {
-  res.send(articles);
+  if (articles.length > 0) {
+    res.send(articles);
+  } else {
+    res.status(404).send();
+  }
 });
 
 router.get("/articles/:id", async (req, res) => {
   const _id = req.params.id;
   const article = articles.filter(({ id }) => id === _id);
-  res.send(article);
+  if (article.length > 0) {
+    res.send(article);
+  } else {
+    res.status(404).send();
+  }
 });
 
 module.exports = router;
